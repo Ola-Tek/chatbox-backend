09d43e3485e396ce33977f50f0a3a85bce79c947
Ola-tek <obisesanolabode10@gmail.com>
2025-09-02 13:14:12 +0100

    Feat: save message

--- chatbox/chat_app/consumers.py
+++ chatbox/chat_app/consumers.py
@@ -270,3 +270,10 @@ class ChatConsumer(AsyncWebsocketConsumer):
         except Conversation.DoesNotExist:
             return False
         
+    @database_sync_to_async
+    async def save_message(self, message, message_type='text'):
+        """save the message on the database"""
+        conversation = Conversation.objects.get(id=self.conversation_id)
+        message = Message.objects.update_or_create
+    
+        
