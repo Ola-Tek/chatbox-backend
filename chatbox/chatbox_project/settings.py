ed6fa8a46581f7e0aefa9ffe55555a3cb6ea4b4e
Ola-tek <obisesanolabode10@gmail.com>
2025-09-16 22:05:08 +0100

    FEAT: Static files collected by whitenoise

--- chatbox/chatbox_project/settings.py
+++ chatbox/chatbox_project/settings.py
@@ -75,6 +75,7 @@ SIMPLE_JWT = {
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
@@ -169,6 +170,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "static/"
+STATIC_ROOT = BASE_DIR / "staticfiles" #where collected statics put files
+
+#Enabling compressed gzip files
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
